06110049e872fe499ef8ff1c4e46fd4e180e3bac
Shubharaj Subedi <git@github.com:Shubharajsubedi/EMS.git>
2026-01-27 21:46:36 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import  numpy as np
+# b=[[1,2,3],[4,5,6],[7,8,9]]
+# print(b)
+# b_array = np.array(b)
+# print(b_array)
+# print(b_array.shape)
+# print(b_array[1,1])
+
+
+
+# b=[[1,2,3],[1,2,3]]
+# c=[[4,5,6],[4,5,6]]
+# b_array=np.array(b)
+# print(b_array.shape)
+# print(b_array.size)
+# print(b_array*np.array(c))
+# print(b_array+np.array(c))
+
+# b=np.random.random(10)
+# print(b)
+#
+# b=np.random.randint(low=0,high=10,size=10)
+# print(b)
+
+
+# a = [[1,2,3,4,4],[5,2,4,5,6]]
+# b = np.array(a)
+# print(b[1:5,2:4],b[0:2,0:2])
